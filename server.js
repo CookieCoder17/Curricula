dbf4a2139f8a6bfa461581e577f9dd965172ce27
Adam Khoukhi <77121453+CookieCoder17@users.noreply.github.com>
2021-10-23 12:46:09 -0400

    Update port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 
